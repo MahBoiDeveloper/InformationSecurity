df30e3bd888afc619da90cc52a44b2302ff390bf
MahBoiDeveloper <mah.boi.developer@gmail.com>
2024-11-12 17:46:35 +0300

    Convert console project to WPF application

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace InformationSecurity
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Registration rg = new Registration();
+            rg.Show();
+            Close();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var s = sender as TextBox;
+            if (s is null) return;
+
+            s.Text = 
+                (s.Text == "Логин") ||
+                (s.Text == "Пароль") ||
+                (s.Text == "Повторите пароль") ?
+                    string.Empty : 
+                    s.Text;
+        }
+
+        private void TextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var s = sender as TextBox;
+            if (s is null) return;
+
+            s.Text =
+                s.Text.Trim() == string.Empty ?
+                    s.Name :
+                    s.Text;
+        }
+    }
+}
---
+++ Registration.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace InformationSecurity
+{
+    /// <summary>
+    /// Логика взаимодействия для Registration.xaml
+    /// </summary>
+    public partial class Registration : Window
+    {
+        public Registration()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            var s = sender as TextBox;
+            if (s is null) return;
+
+            s.Text =
+                (s.Text == "Логин") ||
+                (s.Text == "Пароль") ||
+                (s.Text == "Повторите пароль") ?
+                    string.Empty :
+                    s.Text;
+        }
+    }
+}
